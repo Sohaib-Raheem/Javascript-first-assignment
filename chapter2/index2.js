ba16645aab4b8964df287124cc4a0fb43ecfc5dc
Zaid <zaidbeluch@gmail.com>
2025-08-01 14:34:22 +0500

    All done

---
+++ chapter2/index2.js
@@ -0,0 +1,40 @@
+// CHAPTER = VARIABLE FOR STRING
+
+//QUESTION 1
+var myName;
+
+//QURSTION 2
+var myName = "Sohaib Raheem";
+
+//QUESTION 3
+var message = "Hello world !";
+alert(message);
+
+//QUESTION 4
+var studentName = "Sohaib";
+var studentAge = "21 Years Old";
+var course = "modern web development";
+
+alert(studentName);
+alert(studentAge);
+alert(course);
+
+//QUESTION 5
+var food = "PIZZA \nPIZ \nPIZ \nPI \np";
+alert(food);
+
+// QUESTION 6
+var email = "sohaib@gmail.com";
+alert("My Email Adress is" + " " + email);
+
+//QUESTION 7
+var book = "A smarter way to learn Javascript";
+alert("I am trying to learn from the book" + " " + book);
+
+//QUESTION 8
+document.write("yah! I can write HTML content through Javascript");
+
+//QUESTION 9
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
---
+++ chapter3/index3.js
@@ -0,0 +1,33 @@
+// CHAPTER 3= VARIABLES         FOR NUMBERS | JAVASCRIPT
+
+//QUESTION 1
+var age = 21;
+alert(age);
+
+//QUESTION 2
+var track = 10;
+alert("You have visited the site" + " " + track + " " + "times");
+
+//QUESTION 3
+document.write(" QUESTION 3 <br><br>");
+var birthYear = 2004;
+document.write("My birth year is" + " " + birthYear + "<br>");
+document.write("Data Type of my declared variable is number <br> <br> <br>");
+
+//QUESTION 4
+document.write(" QUESTION 4 <br><br>");
+var user_name = "Sohaib raheem";
+var product_title = "T-Shirt(s)";
+var quantity = 10;
+
+document.write(
+  user_name +
+    " " +
+    "Ordered" +
+    " " +
+    quantity +
+    " " +
+    product_title +
+    " " +
+    " on SMIT Clothing Store"
+);
